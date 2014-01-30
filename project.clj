926cf175138b2967f58eabb3d241754262cb7d3c
getsometoast <james.lewis@7digital.com>
2014-01-30 21:07:06 +0000

    update the project file to include all required dependecies

--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
-(defproject clojure-web-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+(defproject clojure-web-test "0.0.1"
+  :description "Little test project for clojure web development"
+  :url "https://github.com/getsometoast/clojure-web-test.git"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.novemberain/monger "1.7.0"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]
+                 ]
+  :main ^:skip-aot clojure-web-test.core
+  :uberjar-name "webtest-standalone.jar"
+  :profiles {:uberjar {:aot :all}}
+  )
